1ab839676497c1693b96dba48f92963b3b2ab8cd
Cinthia M. Tanaka <cimarie@gmail.com>
2015-05-28 22:50:51 -0300

    Modificacoes reteste2.py

--- retest2.py
+++ retest2.py
@@ -10,8 +10,8 @@ from scipy import sparse
 from collections import Counter
 from itertools import chain,izip
 
-#logging.basicConfig(level=logging.INFO)
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 def gera_simulacao(gnum, inum, pa, b, c, delta, mutacao, alpha, beta, pmig):
