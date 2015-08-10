f5b6809eb15ed822f2a627aa54007f9210dcb9dc
Cinthia Tanaka <cimarie@gmail.com>
2015-08-10 18:40:45 -0300

    melhoria workflow

--- faz_testemc.sh
+++ faz_testemc.sh
@@ -7,9 +7,9 @@ mu="0.0001"
 for b in "0.0" "1.0" "2.0" "5.0"; do
     for alpha in "0.2" "0.8" "1.0"; do 
         cd ~/Documents/simAleatorio
-        python workflow_teste.py RodaTodosTestes --local-scheduler --benefit $b --alpha $alpha --nindiv 26 --ngroup 5000 --mu 0.0001 --delta $delta
-        mv teste_mc_c\=*.txt figuras_mc
-        mv teste_mc.log figuras_mc 
+        python workflow_teste.py RodaTodosTestes --local-scheduler --benefit $b --alpha $alpha --nindiv 26 --ngroup 5000 --mu 0.0001 --delta $delta > resflow_b=${b}.alpha=${alpha}.txt
+        #mv teste_mc_c\=*.txt figuras_mc
+        #mv teste_mc.log figuras_mc 
         cd figuras_mc
         python plotar_fig.py imprime_figuras --b $b --delta $delta --alpha $alpha --n $nindiv
         mkdir b\=${b}_delta\=${delta}_alpha\=${alpha}
